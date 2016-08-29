ea3fabca7f801cb4d825845ea2598cd52ada733f
carlosmori94 <carlosmori34@gmail.com>
2016-08-29 20:22:48 -0300

    Lo que hice hasta el recreo

---
+++ Guiaphp.php
@@ -0,0 +1,34 @@
+><html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+/**
+* 
+*/
+abstract class ClassName
+{
+	protected $_color;
+	protected $_perimetro;
+	protected $_superficie;
+
+	void function __construct()
+	{
+		# code...
+	}
+	//Setter y Getter de color
+	string public function GetterColor()
+	{
+		return $this->_color;
+	}
+	void public function SetterColor($colorNuevo)
+	{
+		$this->_color = $colorNuevo;
+	}
+	abstract protected function
+
+}
+?>
+</body>
+</html>
