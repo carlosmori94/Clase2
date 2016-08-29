7fdaf54c9af2a4b58dca790875aa1a4f0a7fe309
carlosmori94 <carlosmori34@gmail.com>
2016-08-29 19:58:37 -0300

    Primeros dos archivos de la guia

---
+++ Ejercicio1.php
@@ -0,0 +1,17 @@
+><html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+	$suma = 0;
+	$numero = 1 ;
+	do {
+		$suma += $numero;
+		echo"<h2>La suma hasta ahora es de ".$suma."</h2></br>";
+	 	# code...
+	 } while ( $suma <= 1000); 
+
+?>
+</body>
+</html>
---
+++ Ejercico2.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+	$time = time();
+	echo date("d-m-Y (H:i:s)", $time);
+	echo date("d-m-Y (H:i:s)",$time."</br>");
+
+	?>
+
+</body>
+</html>	
